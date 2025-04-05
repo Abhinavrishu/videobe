244cd8b1cbe7df69b6612e63821fc860cd6cd19d
abhinav <mishrakabhinav34@gmail.com>
2025-04-05 10:36:18 +0530

    Update socket URL to deployed backend

--- src/providers/socket.jsx
+++ src/providers/socket.jsx
@@ -5,7 +5,10 @@ const SocketContext = createContext(null);
 export const useSocket = () => useContext(SocketContext);
 
 export const SocketProvider = ({ children }) => {
-  const socket = useMemo(() => io("https://videobackend-n9ro.onrender.com"), []);
+  const socket = useMemo(() =>io("https://videobackend-n9ro.onrender.com", {
+    transports: ["websocket"],
+  })
+  , []);
   return (
     <SocketContext.Provider value={{ socket }}>
       {children}
